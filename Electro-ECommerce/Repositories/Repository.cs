c7ab76c9f6cb9c4ed227d2c7d2fc904f0e08ced1
Eman Elamir <emanelamir929@gmail.com>
2025-02-11 13:18:53 +0200

    Updates

---
+++ Electro-ECommerce/Repositories/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Electro_ECommerce.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context, DbSet<T> dbSet)
+        {
+            _context = context;
+            _dbSet = dbSet;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
